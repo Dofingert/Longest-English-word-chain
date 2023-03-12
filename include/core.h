96caae2f85be76f2a4d02fde2baa99b248f86b32
wz <dofingert@gmail.com>
2023-03-12 23:09:09 +0800

    修改：修改CmakeList.txt 及core.h 以支持动态链接库

--- include/core.h
+++ include/core.h
@@ -5,16 +5,18 @@
 #ifndef CORE_CORE_H
 #define CORE_CORE_H
 
-//extern "C" {
-//__declspec(dllimport) int gen_chains_all(char *words[], int len, char *result[]) noexcept(false);
-//__declspec(dllimport) int
-//gen_chain_word(char *words[], int len, char *result[], char head, char tail, bool allow_circ) noexcept(false);
-//__declspec(dllimport) int gen_chain_word_unique(char *words[], int len, char *result[]) noexcept(false);
-//__declspec(dllimport) int
-//gen_chain_char(char *words[], int len, char *result[], char head, char tail, bool enable_loop) noexcept(false);
-//}
-int gen_chains_all(char *words[], int len, char *result[], void *out_malloc(size_t));
-int gen_chain_word(char *words[], int len, char *result[], char head, char tail, char jail, bool enable_loop, void *out_malloc(size_t));
-int gen_chain_char(char *words[], int len, char *result[], char head, char tail, char jail, bool enable_loop, void *out_malloc(size_t));
+#ifdef EXPORT_FPIC
+#define EXPOSED_FUNCTION extern "C" __declspec(dllexport)
+#else
+#define EXPOSED_FUNCTION extern "C" __declspec(dllimport)
+#endif
+
+EXPOSED_FUNCTION int gen_chains_all(char *words[], int len, char *result[], void *out_malloc(size_t));
+
+EXPOSED_FUNCTION int gen_chain_word(char *words[], int len, char *result[], char head, char tail, char jail, bool enable_loop,
+                   void *out_malloc(size_t));
+
+EXPOSED_FUNCTION int gen_chain_char(char *words[], int len, char *result[], char head, char tail, char jail, bool enable_loop,
+                   void *out_malloc(size_t));
 
 #endif //CORE_CORE_H
