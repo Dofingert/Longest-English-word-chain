3304a5c613c79095b8209555534ceeac2caba994
wz <dofingert@gmail.com>
2023-03-13 16:59:35 +0800

    修正：对拍bug

--- matching_test/matching_test.cpp
+++ matching_test/matching_test.cpp
@@ -21,11 +21,28 @@ void print_all(int result[], int cnt) {
 }
 
 int main(int argc, char *argv[]) {
+    if (argc == 2) {
+        int loop_id;
+        sscanf(argv[1], "%d", &loop_id);
+        FILE *fp = fopen("no_ring.txt", "w");
+        char **random_data = generator(26, true, 26, loop_id + 0 * 65536);
+        for (int cnt = 0; cnt < 26; cnt++) {
+            fprintf(fp, "%s ", random_data[cnt]);
+        }
+        fclose(fp);
+        fp = fopen("with_ring.txt", "w");
+        random_data = generator(6, false, 27, loop_id + 1 * 65536);
+        for (int cnt = 0; cnt < 26; cnt++) {
+            fprintf(fp, "%s ", random_data[cnt]);
+        }
+        fclose(fp);
+        return 0;
+    }
     if (argc < 3) {
         std::cout << "Usage: maching_test dll_under_test{2,} ..." << std::endl;
         return -1;
     }
-    int loop_cnt = 40;
+    int loop_cnt = 4000;
     max_fut_f max_word_func[argc - 1];
     max_fut_f max_char_func[argc - 1];
     max_cnt_f cnt_func[argc - 1];
@@ -40,10 +57,10 @@ int main(int argc, char *argv[]) {
         max_char_func[prog - 1] = (max_fut_f) GetProcAddress(hInstLib[prog - 1], "gen_chain_char");
         cnt_func[prog - 1] = (max_cnt_f) GetProcAddress(hInstLib[prog - 1], "gen_chains_all");
         if (max_word_func[prog - 1] == nullptr) {
-            throw std::logic_error("Cannot load function max_word_func in library: " + std::string(argv[prog]));
+            throw std::logic_error("Cannot load function gen_chain_word in library: " + std::string(argv[prog]));
         }
         if (max_char_func[prog - 1] == nullptr) {
-            throw std::logic_error("Cannot load function max_char_func in library: " + std::string(argv[prog]));
+            throw std::logic_error("Cannot load function gen_chain_char in library: " + std::string(argv[prog]));
         }
         if (cnt_func[prog - 1] == nullptr) {
             throw std::logic_error("Cannot load function cnt_func in library: " + std::string(argv[prog]));
@@ -53,14 +70,15 @@ int main(int argc, char *argv[]) {
     // Main loop, Generate data in every loop.;
     for (int loop_id = 0; loop_id < loop_cnt; loop_id++) {
         // NO RING TEST
-        for (int ring = 0; ring < 2; ring++) {
-            int word_cnt = 10;
+        for (int ring = 0; ring < 1; ring++) {
+            int word_cnt = 4000;
             int cnt_result_table[argc - 1];
             int word_result_table[argc - 1];
             int char_result_table[argc - 1];
             std::set<int> cnt_result_set;
             std::set<int> word_result_set;
             std::set<int> char_result_set;
+#pragma omp parallel for
             for (int i = 0; i < argc - 1; i++) {
                 char **input = generator(26, ring == 0, word_cnt, loop_id + ring * 65536);
                 word_set_t dictionary = build_dictionary(word_cnt, input);
@@ -73,7 +91,9 @@ int main(int argc, char *argv[]) {
                     for (int line = 0; line < link_cnt; line++) {
                         err += check_validation(dictionary, appear, result[line]);
                     }
-                    free(result[0]);
+                    if (link_cnt != 0) {
+                        free(result[0]);
+                    }
                     if (err) {
                         cnt_result_table[i] = -2;
                         cnt_result_set.insert(-2);
@@ -98,7 +118,9 @@ int main(int argc, char *argv[]) {
                         }
                     }
                     err = check_validation(dictionary, appear, result_str.str().c_str());
-                    free(result[0]);
+                    if (link_cnt != 0) {
+                        free(result[0]);
+                    }
                     if (err) {
                         word_result_table[i] = -2;
                         word_result_set.insert(-2);
@@ -123,7 +145,9 @@ int main(int argc, char *argv[]) {
                         }
                     }
                     err = check_validation(dictionary, appear, result_str.str().c_str());
-                    free(result[0]);
+                    if (link_cnt != 0) {
+                        free(result[0]);
+                    }
                     if (err) {
                         char_result_table[i] = -2;
                         char_result_set.insert(-2);
@@ -146,6 +170,15 @@ int main(int argc, char *argv[]) {
                 std::cout << "loop: " << loop_id << " ring: " << ((ring == 1) ? "true " : "false ")
                           << "cnt_result_set mismatch!" << std::endl;
                 print_all(cnt_result_table, argc - 1);
+                FILE *fp = fopen("err_data.txt", "w");
+                char **data = generator(26, ring == 0, word_cnt, loop_id + ring * 65536);
+                for (int i = 0; i < word_cnt; i++) {
+                    fprintf(fp, "%s\n", data[i]);
+                }
+                free(data[0]);
+                free(data);
+                fclose(fp);
+                return -1;
             }
             if (word_result_set.size() == 1 && word_result_set.find(-2) == word_result_set.end()) {
                 std::cout << "loop: " << loop_id << " ring: " << ((ring == 1) ? "true " : "false ")
@@ -154,6 +187,15 @@ int main(int argc, char *argv[]) {
                 std::cout << "loop: " << loop_id << " ring: " << ((ring == 1) ? "true " : "false ")
                           << "word_result_set mismatch!" << std::endl;
                 print_all(word_result_table, argc - 1);
+                FILE *fp = fopen("err_data.txt", "w");
+                char **data = generator(26, ring == 0, word_cnt, loop_id + ring * 65536);
+                for (int i = 0; i < word_cnt; i++) {
+                    fprintf(fp, "%s\n", data[i]);
+                }
+                free(data[0]);
+                free(data);
+                fclose(fp);
+                return -1;
             }
             if (char_result_set.size() == 1 && char_result_set.find(-2) == char_result_set.end()) {
                 std::cout << "loop: " << loop_id << " ring: " << ((ring == 1) ? "true " : "false ")
@@ -162,6 +204,15 @@ int main(int argc, char *argv[]) {
                 std::cout << "loop: " << loop_id << " ring: " << ((ring == 1) ? "true " : "false ")
                           << "char_result_set mismatch!" << std::endl;
                 print_all(char_result_table, argc - 1);
+                FILE *fp = fopen("err_data.txt", "w");
+                char **data = generator(26, ring == 0, word_cnt, loop_id + ring * 65536);
+                for (int i = 0; i < word_cnt; i++) {
+                    fprintf(fp, "%s\n", data[i]);
+                }
+                free(data[0]);
+                free(data);
+                fclose(fp);
+                return -1;
             }
         }
     }
